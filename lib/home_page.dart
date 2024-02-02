83b879005c9f762fae08c3becd8770ecc1421f45
Halex-64 <halexgoncalves6@gmail.com>
2024-02-02 09:08:07 -0300

    continuando a desenvolver a pagina inicial

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,98 +8,152 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pet Snoop',style: GoogleFonts.nunitoSans(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),),
+        title: Text(
+          'Pet Snoop',
+          style: GoogleFonts.nunitoSans(
+              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
         centerTitle: true,
+        actions: const [Icon(Icons.shopping_cart,color: Colors.white,)],
         backgroundColor: Colors.deepPurple,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text(
               'Bem \nVindo(a)',
-              style: GoogleFonts.roboto(fontSize: 30, fontWeight: FontWeight.bold),
-              )
-            ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              'Confira nossos produtos de alta qualidade para atender as necessidades do seu Pet',
-              style: GoogleFonts.roboto(fontSize:18 , fontWeight: FontWeight.normal),
-            ),
+              style:
+                  GoogleFonts.roboto(fontSize: 30, fontWeight: FontWeight.bold),
+            )),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text(
+            'Confira nossos produtos de alta qualidade para atender as necessidades do seu Pet',
+            style:
+                GoogleFonts.roboto(fontSize: 18, fontWeight: FontWeight.normal),
           ),
-          SizedBox(
-            height: 50,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(8),
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: Text('Todos', style: GoogleFonts.roboto(),
-                  ),
+        ),
+        SizedBox(
+          height: 50,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(50),
                 ),
-                Container(
-                  margin: const EdgeInsets.all(8),
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(50)),
-                  child: Row(children: [
-                    Image.asset('assets/images/cat.png', width: 30,height: 30,),
-                    Text('Gatos', style: GoogleFonts.roboto(color: Colors.white))
-                  ]),
+                child: Text(
+                  'Todos',
+                  style: GoogleFonts.roboto(),
                 ),
-                Container(
-                  margin: const EdgeInsets.all(8),
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
+              ),
+              Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
                     color: Colors.black,
                     borderRadius: BorderRadius.circular(50)),
-                    child: Row(children: [
-                      Image.asset('assets/images/dog.png', width: 30, height: 30,),
-                      Text('Cães', style: GoogleFonts.roboto(color: Colors.white)),
-                    ]),
+                child: Row(children: [
+                  Image.asset(
+                    'assets/images/cat.png',
+                    width: 30,
+                    height: 30,
+                  ),
+                  Text('Gatos', style: GoogleFonts.roboto(color: Colors.white))
+                ]),
+              ),
+              Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(50)),
+                child: Row(children: [
+                  Image.asset(
+                    'assets/images/dog.png',
+                    width: 30,
+                    height: 30,
+                  ),
+                  Text('Cães', style: GoogleFonts.roboto(color: Colors.white)),
+                ]),
+              ),
+              Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(50)),
+                child: Row(children: [
+                  Image.asset(
+                    'assets/images/bird.png',
+                    width: 30,
+                    height: 30,
+                  ),
+                  Text('Pássaros',
+                      style: GoogleFonts.roboto(color: Colors.white)),
+                ]),
+              ),
+              Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(50)),
+                child: Row(children: [
+                  Image.asset(
+                    'assets/images/lizard.png',
+                    width: 30,
+                    height: 30,
+                  ),
+                  Text('Outros',
+                      style: GoogleFonts.roboto(color: Colors.white)),
+                ]),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text('Rações',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.arimo(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+        SizedBox(
+          height: 200,
+          child: ListView(scrollDirection: Axis.horizontal, children: [
+            Container(
+              margin: const EdgeInsets.all(8),
+              height: 100,
+              width: 200,
+              child: Card(
+                color: Colors.purple[50],
+                elevation: 4,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Image.asset(
+                        'assets/images/racao3.png',
+                        width: 110,
+                        height: 110,
+                      ),
+                    ),
+                    Text('Ração para gatos Whiskas',
+                        style: GoogleFonts.roboto(
+                            fontSize: 14, fontWeight: FontWeight.bold)),
+                  ],
                 ),
-                Container(
-                  margin: const EdgeInsets.all(8),
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(50)),
-                    child: Row(children: [
-                      Image.asset('assets/images/bird.png', width: 30, height: 30,),
-                      Text('Pássaros', style: GoogleFonts.roboto(color: Colors.white)),
-                    ]),
-                  ),
-                Container(
-                  margin: const EdgeInsets.all(8),
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(50)),
-                    child: Row(children: [
-                      Image.asset('assets/images/lizard.png', width: 30, height: 30,),
-                      Text('Outros', style: GoogleFonts.roboto(color: Colors.white)),
-                    ]),
-                  ),
-              ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Text('Rações', textAlign: TextAlign.center, style: GoogleFonts.arimo(fontSize: 30, fontWeight: FontWeight.bold, )),    
-             ),
-          SizedBox(
-            
-          ),
-        ]
-      ),
+          ]),
+        ),
+      ]),
     );
   }
 }
